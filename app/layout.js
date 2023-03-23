34a8a2183708b637923eb13a2b55695f85864802
melissa banoen garde <m.banoengarde@gmail.com>
2023-03-22 22:48:45 -0400

    npm i react-icons

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Header, Footer } from "@/components";
 
 export const metadata = {
   title: "Worldflags",
@@ -9,7 +10,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
