3d8236fe9ff93264087764ac302f3b94a91f3d11
melissa banoen garde <m.banoengarde@gmail.com>
2023-03-24 18:44:58 -0400

    Set up the base of Authentication context with firebase's user registration

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -3,6 +3,27 @@
 import { createContext, useContext } from "react";
 import { auth } from "@/config/firebase";
 
-export default function AuthContext() {
-  return <div>AuthContext</div>;
-}
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+
+const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  const register = async (name, email, password) => {
+    const userCredentials = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const { user } = userCredentials;
+
+    return await updateProfile(user, { displayName: name });
+  };
+
+  return (
+    <AuthContext.Provider value={{ register }}>{children}</AuthContext.Provider>
+  );
+};
+
+export const Auth = () => {
+  return useContext(AuthContext);
+};
