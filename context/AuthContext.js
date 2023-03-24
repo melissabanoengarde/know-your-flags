e6fa4d44056e596fccfa36a7ea15bf203af45782
melissa banoen garde <m.banoengarde@gmail.com>
2023-03-24 18:04:34 -0400

    Set up firebase configuration

---
+++ context/AuthContext.js
@@ -0,0 +1,8 @@
+"use client";
+
+import { createContext, useContext } from "react";
+import { auth } from "@/config/firebase";
+
+export default function AuthContext() {
+  return <div>AuthContext</div>;
+}
