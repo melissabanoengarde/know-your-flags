756e40463b804f96cc840b6a1be81b213eaa121f
melissa banoen garde <m.banoengarde@gmail.com>
2023-03-24 17:19:26 -0400

    Adjust Card component's height and hover effect

--- components/Card.jsx
+++ components/Card.jsx
@@ -2,20 +2,20 @@ import Image from "next/image";
 
 export default function Card({ country, flagPng, flagAlt }) {
   return (
-    <div className="border border-gray h-[200px] sm:h-[250px] relative">
-      <div className="absolute z-10 w-full h-full duration-300 hover:bg-green-500/10 hover:backdrop-grayscale" />
+    <div className="border border-gray h-[200px] sm:h-[200px] relative hover:border-green-300 duration-300">
+      {/* <div className="absolute z-10 w-full h-full duration-300 hover:backdrop-grayscale hover:bg-green-200/20" /> */}
 
-      <div className="w-full h-[70%] relative border-b-[1px]">
+      <div className="w-full h-[75%] relative border-b-[1px]">
         <Image
           src={flagPng}
           alt={flagAlt ? flagAlt : `Flag of ${country}`}
           fill={true}
           quality={100}
-          className="object-cover w-full h-auto backdrop-grayscale"
+          className="object-cover w-full h-auto"
         />
       </div>
 
-      <div className="h-[30%] w-full flex items-center text-gray-400">
+      <div className="h-[25%] w-full flex items-center text-gray-400">
         <h2 className="px-3 uppercase md:text-xl">{country}</h2>
       </div>
     </div>
