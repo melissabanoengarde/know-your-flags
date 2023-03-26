cc85a44734ffaf19325f68e2834ba59061a16fdb
melissa banoen garde <m.banoengarde@gmail.com>
2023-03-25 21:35:49 -0400

    Update Drawer component for when user is in session or not

--- components/Drawer.jsx
+++ components/Drawer.jsx
@@ -1,8 +1,9 @@
-import React from "react";
 import Link from "next/link";
+import { UserAuth } from "@/context/AuthContext";
 
 export default function Drawer({ open }) {
-  // console.log("d:", open);
+  const { user } = UserAuth();
+
   return (
     <aside
       className={`md:hidden w-full mt-[40px] fixed top-0 right-0 bg-white border-b-[1px] transition-top ease-in-out duration-500 z-10 text-gray-400
@@ -10,7 +11,7 @@ export default function Drawer({ open }) {
     >
       <ul className="text-sm">
         <li className="px-5 py-1 uppercase hover:bg-green-100 cursor-pointer border-b-[1px]">
-          <Link href="/">Play</Link>
+          <Link href="/account">{!user ? "Play" : `${user.displayName}`}</Link>
         </li>
         <li className="px-5 py-1 uppercase cursor-pointer hover:bg-green-100 border-b-[1px]">
           <Link href="/countries">Review</Link>
